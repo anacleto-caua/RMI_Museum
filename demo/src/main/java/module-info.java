02de4fac3d0ad9e6642480e8baf83ff67fda201c
StephanyeCunto <stephanyecristine6@gmail.com>
2025-06-10 20:55:54 -0300

    Correção no module

--- demo/src/main/java/module-info.java
+++ demo/src/main/java/module-info.java
@@ -7,10 +7,8 @@ module com {
 
     opens com to javafx.fxml;
     opens com.controller to javafx.fxml;
-    opens com.view to javafx.fxml;
 
     exports com;
-    exports com.view to javafx.graphics;
     exports com.controller to javafx.fxml;
     exports com.service to java.rmi;
 }
